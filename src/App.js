b90294e4fd052c8bd46df36d1c7e6476d144e440
Sunny Mishra <sunnymishra9007689708@gmail.com>
2024-02-28 00:51:17 +0530

    Created Category container and Category item compnents and styled using sass

---
+++ src/App.js
@@ -0,0 +1,9 @@
+
+import CategoriesContainer from './components/categories-container/categories.component';
+const App = () => {
+  return(
+    <CategoriesContainer/>
+  )
+}
+
+export default App;
